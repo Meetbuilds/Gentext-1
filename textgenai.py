db9612ef35c4a0421a2f4487ad0b5a251f9e6fbc
Meetbuilds <meetdave643@gmail.com>
2025-08-10 19:23:28 +0530

    fixed no emoji problem

--- textgenai.py
+++ textgenai.py
@@ -5,17 +5,17 @@ import datetime
 
 #configuration
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-generation_config = {'temperature':0.9, 'top_p':0.95, 'max_output_tokens':1024}
+generation_config = {'temperature':0.9, 'top_p':0.95, 'max_output_tokens':2000}
  
 
 prompt_file_path = r"C:\Users\meetd\Desktop\Pro1\system_prompt.txt"
 output_directory = r"C:\Users\meetd\Desktop\Pro1\generated_texts"
 
 try:
-    with open(prompt_file_path, "r") as f:
+    with open(prompt_file_path, "r", encoding='utf-8') as f:
         system_prompt = f.read()
 
-    model = genai.GenerativeModel("gemini-pro", generation_config=generation_config)
+    model = genai.GenerativeModel("gemini-2.5-flash", generation_config=generation_config)
 
     response= model.generate_content(system_prompt)
     
@@ -27,7 +27,7 @@ try:
     
     os.makedirs(output_directory, exist_ok=True)
     
-    with open(output_file_path, "w") as f:
+    with open(output_file_path, "w", encoding='utf-8') as f:
         f.write(response_content) 
 
     print(f"Model response successfully saved to: {output_file_path}")
